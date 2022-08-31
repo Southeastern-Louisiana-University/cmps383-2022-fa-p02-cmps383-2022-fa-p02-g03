734c5e5639b1fc52f60a2d6765f29587fb45a481
SimonGrant99 <77806621+SimonGrant99@users.noreply.github.com>
2022-08-31 14:00:29 -0500

    Created Products DTO and Get All Endpoint

---
+++ FA22.P02.Web/DTOs/Products.cs
@@ -0,0 +1,22 @@
+﻿namespace FA22.P02.Web.DTOs
+{
+    public class Products
+    { 
+        public int Id { get; set; }
+        public string? ProductName { get; set; }
+
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+
+    }
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string? ProductName { get; set; }
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+    }
+
+}
--- FA22.P02.Web/Program.cs
+++ FA22.P02.Web/Program.cs
@@ -1,48 +1,52 @@
-var builder = WebApplication.CreateBuilder(args);
+//Connect to Product Data Transfer Object
+using FA22.P02.Web.DTOs;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//Product List
+var products = new List<ProductDto> {
+    new ProductDto {
+        Id = 1,
+        ProductName = "Monster Energy Drink",
+        Description = "caffeine beverage",
+        Price = 3.09m
+    },
+    new ProductDto {
+        Id = 2,
+        ProductName = "Pencil Pack",
+        Description = "pack of 5 mechanical pencils",
+        Price = 6.11m
+    },
+    new ProductDto {
+        Id = 3,
+        ProductName = "M&M's",
+        Description = "regular size pack of m&m's",
+        Price = 1.68m
+    }
 };
 
-app.MapGet("/weatherforecast", () =>
+//Get All Products
+app.MapGet("/api/GetAll", () =>
 {
-    throw new Exception("remove me");
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return products;
 })
-.WithName("GetWeatherForecast");
+.Produces(200, typeof(ProductDto[]));
 
+
+
+//Run App Command
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-
-//see: https://docs.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-6.0
 // Hi 383 - this is added so we can test our web project automatically. More on that later
 public partial class Program { }
